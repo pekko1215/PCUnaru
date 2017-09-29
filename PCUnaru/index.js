c65b1e46ded3e66d72b224c0c40367a79134739a
pekko1215 <pekko1215@yahoo.co.jp>
2017-09-30 01:53:30 +0900

    dwa

--- PCUnaru/index.js
+++ PCUnaru/index.js
@@ -14,8 +14,24 @@ injectScript(function() {
             };
         } catch (e) {}
     };
+
+    var changePlatfome = (ua) => {
+        // Chrome, fx, IE11
+        window.navigator.__defineGetter__('platform', () => ua);
+        console.log(window.platform)
+        // Safari
+        try {
+            // fxでsetterがないとエラーになるので
+            window.platform = {
+                get platform() {
+                    return ua;
+                }
+            };
+        } catch (e) {}
+    };
     console.log(window.GameBase)
-    changeUserAgent("Win NT 10 ChromeベーススーパーブラウザーＸ");
+    changeUserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36 Mac OS X .1_");
+    changePlatfome("Win32")
     //必要なものは根気と執着、あとほんのちょっとのお茶目ささ
 })
 
